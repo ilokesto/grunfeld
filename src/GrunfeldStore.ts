5894275b714f59908c73446928bda6fc41cc2243
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-07-01 20:49:26 +0900

    feat: IGrunfeldProps 인터페이스 추가 및 GrunfeldStore 메서드 리팩토링

--- src/GrunfeldStore.ts
+++ src/GrunfeldStore.ts
@@ -1,46 +1,84 @@
 import { Position } from "./types";
 
-type Store = Array<{element: React.ReactNode, position?: Position}>;
+export interface IGrunfeldProps {
+  element: React.ReactNode;
+  position?: Position;
+  lightDismiss?: boolean;
+  dismissCallback?: () => unknown;
+}
+
+type Store = Array<IGrunfeldProps>;
 
 export class GrunfeldStore {
   static _callbacks = new Set<() => void>();
-  static _store: Store = []
+  static _store: Store = [];
 
-  static addDialog = (dialog: {element: React.ReactNode, position?: Position}) => {
-    this._store.push(dialog);
-    this._callbacks.forEach((ls) => ls());
-  }
+  static add = (dialog: IGrunfeldProps) => {
+    GrunfeldStore._store.push(dialog);
+    GrunfeldStore._callbacks.forEach((ls) => ls());
+  };
 
-  static removeDialog = () => {
-    this._store.pop();
-    this._callbacks.forEach((ls) => ls());
-  }
+  static addAsync = <T>(
+    dialog: (removeWith: (data: T) => T) => IGrunfeldProps
+  ): Promise<T> => {
+    return new Promise<T>((resolve) => {
+      const removeWith = (data: T) => {
+        // dialog로 생성된 props를 스토어에서 제거
+        GrunfeldStore._store = GrunfeldStore._store.filter(
+          (d) => d !== dialogProps
+        );
+        GrunfeldStore._callbacks.forEach((ls) => ls());
+        // remove가 호출될 때 data를 resolve
+        resolve(data);
+        return data;
+      };
 
-  static clearStore = () => {
-    this._store = [];
-    this._callbacks.forEach((ls) => ls());
-  }
+      const dialogProps = dialog(removeWith);
+      GrunfeldStore._store.push(dialogProps);
+      GrunfeldStore._callbacks.forEach((ls) => ls());
+    });
+  };
+
+  static remove = () => {
+    const props = GrunfeldStore._store.pop();
+
+    if (props?.dismissCallback) props.dismissCallback();
+
+    GrunfeldStore._callbacks.forEach((ls) => ls());
+  };
+
+  static clear = () => {
+    GrunfeldStore._store.forEach((props) => {
+      if (props.dismissCallback) props.dismissCallback();
+    });
+
+    GrunfeldStore._store = [];
+
+    GrunfeldStore._callbacks.forEach((ls) => ls());
+  };
 
   static get store() {
-    return this._store;
+    return GrunfeldStore._store;
   }
 
   static addListener(listener: () => void) {
-    this._callbacks.add(listener);
+    GrunfeldStore._callbacks.add(listener);
   }
 
   static removeListener(listener: Function) {
-    this._callbacks = new Set(Array.from(this._callbacks).filter((l) => l !== listener));
+    GrunfeldStore._callbacks = new Set(
+      Array.from(GrunfeldStore._callbacks).filter((l) => l !== listener)
+    );
   }
 
   static isStoreEmpty() {
-    return Object.keys(this.store).length === 0;
+    return Object.keys(GrunfeldStore.store).length === 0;
   }
 }
 
-
 export default {
-  add: GrunfeldStore.addDialog,
-  remove: GrunfeldStore.removeDialog,
-  clear: GrunfeldStore.clearStore
-}
+  add: GrunfeldStore.add,
+  addAsync: GrunfeldStore.addAsync,
+  remove: GrunfeldStore.remove,
+  clear: GrunfeldStore.clear,
+};
