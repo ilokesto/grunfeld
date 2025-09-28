f247ec1016a627c0f456722b73fe42cce6a2f04a
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-09-28 19:56:55 +0900

    feat: GrunfeldDialog 및 GrunfeldModal에서 BaseGrunfeld 제거 및 useDialog 훅 추가

--- src/components/GrunfeldModal.tsx
+++ src/components/GrunfeldModal.tsx
@@ -1,27 +1,43 @@
+import { useRef } from "react";
+import { useFocusManagement, useGrunfeldBehavior } from "../hooks";
 import { GrunfeldElementProps } from "../types";
-import { getModalBackdropStyles } from "../utils";
-import { BaseGrunfeld } from "./BaseGrunfeld";
+import { getModalBackdropStyles, getPositionStyles } from "../utils";
 
 export function GrunfeldModal(props: GrunfeldElementProps) {
+  const {
+    element,
+    position = "center",
+    lightDismiss = true,
+    backdropStyle,
+  } = props;
+
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  // 공통 행동 처리 (ESC, backdrop click)
+  const { handleBackdropClick } = useGrunfeldBehavior({
+    lightDismiss,
+  });
+
+  // 포커스 관리
+  useFocusManagement(elementRef);
+
+  // 위치 스타일 계산
+  const positionStyles = getPositionStyles(position);
+
   return (
-    <BaseGrunfeld
-      {...props}
-      renderStrategy={({ elementRef, handleBackdropClick, positionStyles }) => (
-        <div
-          style={getModalBackdropStyles(props.backdropStyle)}
-          onClick={handleBackdropClick}
-        >
-          <div
-            ref={elementRef as React.RefObject<HTMLDivElement>}
-            style={positionStyles}
-            role="dialog"
-            aria-modal="true"
-            tabIndex={-1}
-          >
-            {props.element}
-          </div>
-        </div>
-      )}
-    />
+    <div
+      style={getModalBackdropStyles(backdropStyle)}
+      onClick={handleBackdropClick}
+    >
+      <div
+        ref={elementRef}
+        style={positionStyles}
+        role="dialog"
+        aria-modal="true"
+        tabIndex={-1}
+      >
+        {element}
+      </div>
+    </div>
   );
 }
